a2bfb9db0270ba696f081a014f57139bf6321b91
soohyun_kim5 <93cherry@naver.com>
2022-10-26 12:04:38 +0900

    mongodb and mongoose

--- src/server.js
+++ src/server.js
@@ -1,35 +1,18 @@
 import express from "express";
-
-const PORT = 4000;
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next();
-};
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue.")
-    next();
-};
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.")
-};
-const handleHome = (req, res) => {
-    return res.send("I love middlewares.");
-};
-
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleListening = () => console.log(`Server listening on port ${PORT}`);
-
-app.listen(PORT, handleListening);
-
-
+export default app;
